a1cdcb97740c0f3e68412b4ef5d935775d48c76b
MBOEDIK65 <boediksm65@gmail.com>
2026-05-31 18:47:46 +0700

    merge: main - feat/us-1.1-pendaftaran-siswa-baru

---
+++ src/controllers/guruController.js
@@ -0,0 +1,5 @@
+// src/controllers/guruController.js
+
+module.exports = {
+    // Controller functions for Guru will go here
+};
---
+++ src/routes/guruRoutes.js
@@ -0,0 +1,10 @@
+// src/routes/guruRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const guruController = require('../controllers/guruController');
+
+// Define guru routes here
+// router.get('/', guruController.getAllGuru);
+
+module.exports = router;
